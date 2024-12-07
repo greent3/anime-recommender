0a9761601e174bb3a7dccb318aa651364c4805d3
greent3 <ravisbean@gmail.com>
2024-12-07 15:12:34 -0500

    feat(webhook): add user creation webhook

--- apps/nextjs/src/pages/api/webhook.ts
+++ apps/nextjs/src/pages/api/webhook.ts
@@ -1,45 +1,71 @@
-import type { IncomingHttpHeaders } from 'http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { WebhookRequiredHeaders } from 'svix';
-import type { WebhookEvent } from '@clerk/nextjs/server';
-import { Webhook } from 'svix';
-import { prisma } from '@acme/db';
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { UserJSON, WebhookEvent } from "@clerk/nextjs/api";
 
-const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+const prisma = new PrismaClient();
 
 export default async function handler(
-    req: NextApiRequestWithSvixRequiredHeaders,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    const payload = JSON.stringify(req.body);
-    const headers = req.headers;
-    // Create a new Webhook instance with your webhook secret
-    const wh = new Webhook(webhookSecret);
-
-    let evt: WebhookEvent;
+  if (req.method === "POST") {
     try {
-        // Verify the webhook payload and headers
-        evt = wh.verify(payload, headers) as WebhookEvent;
-    } catch (_) {
-        // If the verification fails, return a 400 error
-        return res.status(400).json({});
-    }
-    const { id } = evt.data;
+      const event = req.body as WebhookEvent; // Extract `data` from the request body
 
-    const eventType = evt.type;
-    if (eventType === 'user.created') {
-        await prisma?.user.create({
+      // Basic validation
+      if (!event || !event.data.id) {
+        return res.status(400).json({ error: "User data with id is required" });
+      }
+
+      if (event.type != "user.created") {
+        return;
+      }
+
+      const userJSON = event.data as UserJSON;
+
+      // Run a Prisma transaction to ensure atomic operations
+      await prisma.$transaction(async (tx) => {
+        // Check if the user already exists
+        const existingUser = await tx.user.findUnique({
+          where: {
+            id: userJSON.id,
+          },
+        });
+
+        // If the user doesn't exist, create a new user and proceed
+        if (!existingUser) {
+          const { id } = userJSON;
+          const { username } = userJSON;
+          const emailAddress = userJSON.email_addresses[0]?.email_address;
+
+          if (!id || !username || !emailAddress) {
+            return res.status(400).json({
+              error:
+                "Invalid webhook data. Id, Username, or Email not provided.",
+            });
+          }
+          const createdUser = await prisma?.user.create({
             data: {
-                id: id,
-                username: evt.data.username,
-                email: evt.data.email_addresses[0].email_address
-            }
-        })
-        return res.status(201).json({});
-    }
-    return res.status(404).json({})
-}
+              id: id,
+              username: username,
+              email: emailAddress,
+              createdAt: new Date(),
+            },
+          });
 
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-    headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-};
+          // Return success response
+          return res.status(201).json({ message: "User created successfully" });
+        } else {
+          // If user already exists
+          return res.status(400).json({ error: "User already exists" });
+        }
+      });
+    } catch (err) {
+      console.error("Error creating user and mapping reviewableCards:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    // Method Not Allowed
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
