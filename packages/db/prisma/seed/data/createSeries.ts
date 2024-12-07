79e868c10b5f06db3ef7c9a829856544857f3723
greent3 <ravisbean@gmail.com>
2024-12-07 15:12:56 -0500

    feat(seed): add seeding functionality

--- packages/db/prisma/seed/data/createSeries.ts
+++ packages/db/prisma/seed/data/createSeries.ts
@@ -1,28 +1,34 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 const createSeries = async (prisma: PrismaClient) => {
-    try {
-        const result = await prisma.series.createMany({
-            data: [
-                // User 1
-                {
-                    id: 0,
-                    title: 'Bleach: Thousand Year Blood War',
-                    bio: 'In this arc, the quincies return with....'
-                },
-                // User 2
-                {
-                    id: 1,
-                    title: 'One Piece',
-                    bio: 'With the power of the gumgum fruit on his side, Monkey D. Loofy and his comrades set sail for the grand...'
-                },
-            ],
-        })
+  try {
+    const result = await prisma.series.createMany({
+      data: [
+        {
+          //   id: 0,
+          title: "Bleach: Thousand Year Blood War",
+          bio: "In this arc, the quincies return with....",
+          type: "TV",
+          episodes: 24,
+          airDate: "22 Dec 2021",
+          imgPath: "/bleach.jpeg",
+        },
+        {
+          //   id: 1,
+          title: "One Piece",
+          bio: "With the power of the gumgum fruit on his side, Monkey D. Loofy and his comrades set sail for the grand...",
+          type: "TV",
+          episodes: 1119,
+          airDate: "11 Apr 2001",
+          imgPath: null,
+        },
+      ],
+    });
 
-        return Promise.resolve({ name: "series", data: result })
-    } catch (error) {
-        return Promise.reject(error)
-    }
-}
+    return Promise.resolve({ name: "series", data: result });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-export default createSeries
+export default createSeries;
--- packages/db/prisma/seed/seed.ts
+++ packages/db/prisma/seed/seed.ts
@@ -1,32 +1,26 @@
 import createSeries from "./data/createSeries";
-import createUsers from "./data/createUsers";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
+import { prisma } from "../../";
 
 async function main() {
-    const seeds = [
-        createSeries,
-        createUsers
-    ]
+  const seeds = [createSeries];
 
-    const results: { name: string; data: object }[] = []
+  const results: { name: string; data: object }[] = [];
 
-    for (const seed of seeds) {
-        const response = (await seed(prisma)) as { name: string; data: object }
-        results.push(response)
-    }
+  for (const seed of seeds) {
+    const response = (await seed(prisma)) as { name: string; data: object };
+    results.push(response);
+  }
 
-    results.forEach((result) => {
-        console.info(`Create ${result.name}:`, result.data)
-    })
+  results.forEach((result) => {
+    console.info(`Create ${result.name}:`, result.data);
+  });
 }
 
 main()
-    .catch((e) => {
-        console.error(e)
-        process.exit(1)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
