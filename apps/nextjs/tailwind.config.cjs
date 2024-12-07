a42e02960bd322944cd4b28fd6e8fc15fda9f94c
greent3 <ravisbean@gmail.com>
2024-12-07 15:12:06 -0500

    fix(packages): update packages and turbo commands

--- apps/nextjs/tailwind.config.cjs
+++ apps/nextjs/tailwind.config.cjs
@@ -1,20 +1,37 @@
 /** @type {import("tailwindcss").Config} */
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   presets: [require("@acme/tailwind-config")],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'lightPrimary': '#f6f6f6',
-        'lightSecondary': '#F47521',
-        'lightTertiary': '#808080',
-        'darkPrimary': '#272626',
-        'darkSecondary': '#F47521',
-        'darkTertiary': '#f5f5f5',
-      }
-    }
-
+        ...colors,
+        lightPrimary: "#f6f6f6",
+        lightSecondary: "#F47521",
+        lightTertiary: "#808080",
+        darkPrimary: "#272626",
+        darkSecondary: "#F47521",
+        darkTertiary: "#f5f5f5",
+      },
+    },
   },
   plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       light: {
+  //         ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+  //         primary: "#F47521",
+  //         secondary: "#808080",
+  //       },
+  //       dark: {
+  //         ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+  //         primary: "#272626",
+  //         secondary: "#F47521",
+  //       },
+  //     },
+  //   ],
+  // },
 };
