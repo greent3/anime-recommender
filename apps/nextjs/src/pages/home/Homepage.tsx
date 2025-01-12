193ccc42bb4d6d22ce202dbc36d0e53f80fbe62d
greent3 <ravisbean@gmail.com>
2025-01-12 06:35:34 -0500

    improvement(frontend): add loading spinner and error page to home screen

--- apps/nextjs/src/pages/home/Homepage.tsx
+++ apps/nextjs/src/pages/home/Homepage.tsx
@@ -3,66 +3,69 @@ import InquiryButtonRow from "../../components/buttons/InquiryButtonRow";
 import RatingButtonRow from "../../components/buttons/RatingButtonRow";
 import { SeriesDetails } from "../../components/static/SeriesDetails";
 import { trpc } from "../../utils/trpc";
-import LoadingButton from "../../components/static/LoadingButton";
+import LoadingSpinner from "../../components/static/LoadingSpinner";
 import AddToWatchlistButtonRow from "../../components/buttons/AddToWatchlistButtonRow";
+import { LoadingError } from "../../components/static/LoadingError";
 
 type PageString = "inquiry" | "rating" | "addAction";
 
 function Homepage() {
-  const exampleSeries = trpc.series.getRecommendation.useQuery();
+  const exampleSeries = trpc.series.getRecommendation.useQuery(undefined, {
+    staleTime: Infinity,
+  });
 
-  const [pageToShow, setPageToShow] = useState<PageString>("inquiry");
+  const [buttonsToShow, setButtonsToShow] = useState<PageString>("inquiry");
 
   function goToInquiry() {
-    setPageToShow("inquiry");
+    setButtonsToShow("inquiry");
   }
   function goToRating() {
-    setPageToShow("rating");
+    setButtonsToShow("rating");
   }
   function goToWatchlist() {
-    setPageToShow("addAction");
+    setButtonsToShow("addAction");
   }
 
   return (
     <div className=" themed-centered-stack-fullwidth-fullheight pt-12">
-      {exampleSeries.isFetching && <LoadingButton />}
+      {exampleSeries.isFetching ? (
+        <LoadingSpinner />
+      ) : exampleSeries.data ? (
+        <>
+          <SeriesDetails
+            title={exampleSeries.data.title || "Unknown Title"}
+            type={exampleSeries.data.type}
+            bio={exampleSeries.data.bio}
+            episodes={exampleSeries.data.episodes}
+            aired={exampleSeries.data.startDate}
+            score={String(exampleSeries.data.score)}
+            imgPath={exampleSeries.data.imgPath}
+            categories={exampleSeries.data.categories}
+            trailerUrl={exampleSeries.data.trailerUrl}
+          />
 
-      {!exampleSeries.isFetching && exampleSeries.data && (
-        <SeriesDetails
-          title={exampleSeries?.data?.title || "Unknown Title"}
-          type={exampleSeries?.data?.type}
-          bio={exampleSeries?.data?.bio}
-          episodes={exampleSeries?.data?.episodes}
-          aired={exampleSeries?.data?.airDate}
-          score={String(exampleSeries?.data?.score)}
-          imgPath={exampleSeries?.data?.imgPath}
-          categories={exampleSeries?.data?.categories}
-        />
+          {buttonsToShow == "inquiry" && (
+            <InquiryButtonRow
+              goToRating={goToRating}
+              goToWatchlist={goToWatchlist}
+            />
+          )}
+          {buttonsToShow == "addAction" && (
+            <AddToWatchlistButtonRow
+              goToInquiry={goToInquiry}
+              seriesId={exampleSeries.data.id}
+            />
+          )}
+          {buttonsToShow == "rating" && (
+            <RatingButtonRow
+              goToInquiry={goToInquiry}
+              seriesId={exampleSeries.data.id}
+            />
+          )}
+        </>
+      ) : (
+        <LoadingError />
       )}
-      {pageToShow == "inquiry" &&
-        !exampleSeries.isFetching &&
-        exampleSeries.data && (
-          <InquiryButtonRow
-            goToRating={goToRating}
-            goToWatchlist={goToWatchlist}
-          />
-        )}
-      {pageToShow == "addAction" &&
-        !exampleSeries.isFetching &&
-        exampleSeries.data && (
-          <AddToWatchlistButtonRow
-            goToInquiry={goToInquiry}
-            seriesId={exampleSeries.data?.id}
-          />
-        )}
-      {pageToShow == "rating" &&
-        !exampleSeries.isFetching &&
-        exampleSeries.data && (
-          <RatingButtonRow
-            goToInquiry={goToInquiry}
-            seriesId={exampleSeries.data?.id}
-          />
-        )}
     </div>
   );
 }
