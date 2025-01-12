02a1ba683bbb57c9249336753029dd55108f2bf4
greent3 <ravisbean@gmail.com>
2025-01-12 06:29:23 -0500

    feat(frontend): add loading spinner to main page

--- apps/nextjs/src/components/layout/Layout.tsx
+++ apps/nextjs/src/components/layout/Layout.tsx
@@ -2,26 +2,35 @@ import React, { PropsWithChildren } from "react";
 import { Navbar } from "../navigation/navbar";
 import Head from "next/head";
 import { useAuth } from "@clerk/nextjs";
+import LoadingSpinner from "../static/LoadingSpinner";
 
 const Layout = ({ children }: PropsWithChildren) => {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
   return (
     <>
-      <Head>
-        <title>Anime Recommender</title>
-        <meta
-          name="description"
-          content="Web application for recommending Anime"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex h-screen  flex-col items-center ">
-        {isSignedIn && <Navbar />}
-        <div className=" bg-lightPrimary dark:bg-darkPrimary  h-full  w-full  ">
-          {children}
+      {!isLoaded ? (
+        <div className=" bg-lightPrimary dark:bg-darkPrimary flex h-svh w-full  items-center justify-center align-middle">
+          <LoadingSpinner />
         </div>
-      </main>
+      ) : (
+        <>
+          <Head>
+            <title>Anime Recommender</title>
+            <meta
+              name="description"
+              content="Web application for recommending Anime"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <main className="flex h-screen  flex-col items-center ">
+            {isSignedIn && <Navbar />}
+            <div className=" bg-lightPrimary dark:bg-darkPrimary  h-full  w-full  ">
+              {children}
+            </div>
+          </main>
+        </>
+      )}
     </>
   );
 };
