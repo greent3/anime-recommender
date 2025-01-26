a8050033e5eb13ff4aea66a61de41fbf339104b8
greent3 <ravisbean@gmail.com>
2025-01-26 17:56:14 -0500

    fix(frontend): remove unnecessary parent fragment

--- apps/nextjs/src/components/navigation/navbar/index.tsx
+++ apps/nextjs/src/components/navigation/navbar/index.tsx
@@ -26,36 +26,34 @@ export const Navbar = () => {
   ];
 
   return (
-    <>
-      <div className="bg-lightPrimary dark:bg-darkPrimary flex  h-20 w-full flex-row items-center justify-center pt-2">
-        <div className="container mx-auto px-4 ">
-          <div className="flex items-center justify-between  ">
-            <ThemeIcon />
-            <ul className="hidden gap-x-20 md:flex">
-              {pageNames.map((p, index) => (
-                <li key={`navtab-${index}`}>
-                  <Link href={p.url} className="group">
-                    <p className=" text-lightSecondary dark:text-darkSecondary text-2xl font-semibold ">
-                      {p.name}
-                    </p>
-                    <div className=" bg-lightSecondary dark:bg-darkSecondary h-1 w-full opacity-0 group-hover:opacity-100" />
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <UserButton
-              appearance={{
-                elements: {
-                  userButtonAvatarBox: {
-                    width: "3rem",
-                    height: "3rem",
-                  },
+    <div className="bg-lightPrimary dark:bg-darkPrimary flex  h-20 w-full flex-row items-center justify-center pt-2">
+      <div className="container mx-auto px-4 ">
+        <div className="flex items-center justify-between  ">
+          <ThemeIcon />
+          <ul className="hidden gap-x-20 md:flex">
+            {pageNames.map((p, index) => (
+              <li key={`navtab-${index}`}>
+                <Link href={p.url} className="group">
+                  <p className=" text-lightSecondary dark:text-darkSecondary text-2xl font-semibold ">
+                    {p.name}
+                  </p>
+                  <div className=" bg-lightSecondary dark:bg-darkSecondary h-1 w-full opacity-0 group-hover:opacity-100" />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonAvatarBox: {
+                  width: "3rem",
+                  height: "3rem",
                 },
-              }}
-            />
-          </div>
+              },
+            }}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
